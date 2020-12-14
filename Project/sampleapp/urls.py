d0146f6e89c90848d950ef8690f9776e4a3b2cf5
s1ntaxe770r <cyberdev01@protonmail.com>
2020-12-14 10:10:23 +0100

    integrates celery

--- Project/sampleapp/urls.py
+++ Project/sampleapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.index, name='index'),
+    path("home/", views.index, name="index"),
+    path("mail/", views.mail, name="mail"),
+
 ]
--- Project/sampleapp/views.py
+++ Project/sampleapp/views.py
@@ -1,6 +1,13 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from .tasks import email_me
 
 # Create your views here.
 
-def index(request): 
-    return render(request,"home.html")
+
+def index(request):
+    return render(request, "index.html")
+
+def mail(request):
+    email_me.delay()
+    return HttpResponse("Email sent have a nice day :)")
