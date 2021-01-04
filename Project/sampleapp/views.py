e16dfd4a27c93920317787b0d428c6999ad3c9a8
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-04 07:50:32 +0100

    updates view

--- Project/sampleapp/views.py
+++ Project/sampleapp/views.py
@@ -1,5 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+import requests
 from .tasks import email_me
 
 # Create your views here.
@@ -9,5 +10,6 @@ def index(request):
     return render(request, "index.html")
 
 def mail(request):
+    raise requests.exceptions.HTTPError
     email_me.delay()
     return HttpResponse("Email sent have a nice day :)")
