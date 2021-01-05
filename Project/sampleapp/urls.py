83f775ff05a3975d1fa00bd6c2454859d1017c76
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-05 14:56:22 +0100

    remove index route

--- Project/sampleapp/urls.py
+++ Project/sampleapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.index, name="index"),
     path("mail/", views.mail, name="mail"),
 
 ]
--- Project/sampleapp/views.py
+++ Project/sampleapp/views.py
@@ -6,9 +6,6 @@ from .tasks import email_me
 # Create your views here.
 
 
-def index(request):
-    return render(request, "index.html")
-
 def mail(request):
     email_me.delay()
     return HttpResponse("Email sent have a nice day :)")
