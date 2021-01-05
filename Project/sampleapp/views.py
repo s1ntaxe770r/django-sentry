a05a49c7532c9f8fedc996e1f041b0d540cd320b
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-05 16:56:13 +0100

    form validation

--- Project/sampleapp/views.py
+++ Project/sampleapp/views.py
@@ -2,10 +2,17 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 import requests
 from .tasks import email_me
-
+from .forms import ContactForm
 # Create your views here.
 
 
 def mail(request):
-    email_me.delay()
+    if request.Method == 'POST':
+        form = ContactForm
+        if form.is_valid:
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            email_me.delay(message)
+
+  
     return HttpResponse("Email sent have a nice day :)")
