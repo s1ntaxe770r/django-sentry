cb5fa2926a9f89eac40576325a2dd65a51e4f37c
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-06 11:58:29 +0100

    fix form.cleaned_data

--- Project/sampleapp/views.py
+++ Project/sampleapp/views.py
@@ -3,20 +3,20 @@ from django.shortcuts import render
 import requests
 from .tasks import email_me
 from .forms import ContactForm
-# Create your views here.
+
 
 
 def mail(request):
     if request.method == 'POST':
-        form = ContactForm
-        if form.is_valid:
+        form = ContactForm(request.POST)
+        if form.is_valid():
             subject = "Someone has something to say"
             message = form.cleaned_data['message']
             email_me.delay(message,subject)
             return HttpResponse("Email sent have a nice day :)")
     else:
         form = ContactForm
-    return render(request,'index.html',{'form':form})
+        return render(request,'index.html',{'form':form})
 
 
         
