8dae91b21bb162b554cc0224cb762aacd2a4080b
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-07 22:31:49 +0100

    fix weird format issue and simplify routes

--- Project/SentryApp/urls.py
+++ Project/SentryApp/urls.py
@@ -17,4 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = [path("admin/", admin.site.urls), path("app/", include("sampleapp.urls"))]
+urlpatterns = [
+path("admin/", admin.site.urls), 
+path("/", include("sampleapp.urls"))
+]
